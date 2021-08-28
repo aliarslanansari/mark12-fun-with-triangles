8b20776639091113cce088938b87b895211bb82a
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-28 23:01:24 +0530

    added quiz on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages'
 import IsTriangle from './pages/IsTriangle'
 import Hypotenuse from './pages/Hypotenuse'
 import AreaOfTriangle from './pages/AreaOfTriangle'
@@ -13,9 +12,8 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Home} />
+        <Route path='/' exact component={Quiz} />
         <Route path='/about' component={IsTriangle} />
-        <Route path='/quiz' component={Quiz} />
         <Route path='/hypotenuse' component={Hypotenuse} />
         <Route path='/team' component={AreaOfTriangle} />
       </Switch>
