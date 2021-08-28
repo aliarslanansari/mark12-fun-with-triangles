336e5d9ce341ed076c8207c73185aa4819a08c9d
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-28 23:01:06 +0530

    added quiz data

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const quizQuestions = [
+  {
+    question:
+      'What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?',
+    answer: '90°',
+    options: ['45°', '90°', '60°'],
+  },
+  {
+    question:
+      'What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?',
+    answer: 'right angled',
+    options: ['obtuse', 'acute', 'right angled'],
+  },
+  {
+    question: 'A triangle can have',
+    answer: 'one right angle',
+    options: ['one right angle', 'two right angles'],
+  },
+  {
+    question: 'Which of the following can form a right angled triangle?',
+    answer: '12, 16, 20',
+    options: ['14, 15, 26', '12, 16, 20'],
+  },
+  {
+    question: 'Which of the following triangles are always similar?',
+    answer: 'Equilateral triangle',
+    options: ['Equilateral triangle', 'Isosceles triangle'],
+  },
+  {
+    question:
+      'If one angle of a triangle is obtuse, then which one of the following is the possible measure of remaining angles?',
+    answer: '100°',
+    options: ['100°', '85°'],
+  },
+  {
+    question:
+      'If the largest angle in a triangle is 70°, what is the least possible value of the smallest angle of the triangle?',
+    answer: '30°',
+    options: ['30°', '10°'],
+  },
+  {
+    question: 'The perimeter of scalene triangle with sides a, b, c is',
+    answer: 'a + b + c',
+    options: ['a + b + c', '2a', 'None of these'],
+  },
+  {
+    question: 'A scalene triangle has ___ lines of symmetry',
+    answer: 'no',
+    options: ['two', 'no', '15'],
+  },
+  {
+    question:
+      'There is a right triangle PQR where: angle Q = 90°; angle P = angle R. What is the measure of angles P and R?',
+    answer: '45°',
+    options: ['85°', '65°', '45°'],
+  },
+]
